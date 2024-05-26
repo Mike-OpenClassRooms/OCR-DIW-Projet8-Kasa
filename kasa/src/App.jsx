6e99a05879d2fbb886043877c21afd790bcde92e
Mike Pixel Dev <86058062+mikepixeldev@users.noreply.github.com>
2024-05-26 09:07:49 +0200

    Préparation pages & Sass

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import HousingPage from './pages/HousingPage';
+import Error404Page from './pages/Error404Page';
+import Layout from './components/Layout';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/housing/:id" element={<HousingPage />} />
+          <Route path="*" element={<Error404Page />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
