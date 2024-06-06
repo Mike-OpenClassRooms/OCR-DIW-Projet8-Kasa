9316e20e56e7ad799789b9c0c44e5d4634808419
Mike Pixel Dev <86058062+mikepixeldev@users.noreply.github.com>
2024-06-06 14:11:47 +0200

    Debug problème avec des promesses

--- kasa/src/index.js
+++ kasa/src/index.js
@@ -12,6 +12,15 @@ root.render(
   </React.StrictMode>,
 );
 
+// Ajout d'un gestionnaire global pour les promesses non gérées
+window.addEventListener('unhandledrejection', (event) => {
+  console.error(
+    'Rejet de promesse non géré :',
+    event.reason || 'Aucune raison fournie',
+  );
+  console.log('Détails de la promesse :', event.promise);
+});
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
