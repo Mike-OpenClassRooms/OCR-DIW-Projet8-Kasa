7b2b4f0643a9963e101136b4b83cddb168b7b95a
Mike Pixel Dev <86058062+mikepixeldev@users.noreply.github.com>
2024-06-07 23:41:27 +0200

    Ajout du hook useLocation

--- kasa/src/components/Layout/Layout.jsx
+++ kasa/src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
-// Importation de 'Link' depuis 'react-router-dom' pour gérer les liens de navigation
-import { Link } from 'react-router-dom';
+// Importation de 'Link' et 'useLocation' depuis 'react-router-dom' pour gérer les liens de navigation
+import { Link, useLocation } from 'react-router-dom';
 
 // Importation du fichier SCSS pour styliser le composant Layout
 import './Layout.scss';
@@ -10,10 +10,8 @@ import logoFooter from '../../assets/images/logo-kasa-white.svg';
 
 // Définition du composant fonctionnel Layout qui accepte des props, ici notamment 'children'
 function Layout({ children }) {
-  // Fonction pour gérer les clics sur les liens de navigation
-  const handleNavClick = (label) => {
-    console.log(`Clique sur ${label}`);
-  };
+  // Utilisation du hook useLocation pour obtenir l'objet location
+  const location = useLocation();
 
   return (
     // La div principale qui englobe l'ensemble du layout
@@ -22,21 +20,29 @@ function Layout({ children }) {
       <header>
         {/* Logo et navigation */}
         <div className="header-content">
-          {/* Logo */}
-          <img src={logo} alt="Kasa logo" className="logo" />
+          {/* Logo enveloppé dans un lien */}
+          <Link to="/">
+            <img src={logo} alt="Kasa logo" className="logo" />
+          </Link>
           {/* Barre de navigation */}
           <nav>
             {/* Liste des éléments de navigation */}
             <ul>
               {/* Élément de navigation vers la page d'accueil */}
               <li>
-                <Link to="/" onClick={() => handleNavClick('Accueil')}>
+                <Link
+                  to="/"
+                  className={location.pathname === '/' ? 'active' : ''}
+                >
                   Accueil
                 </Link>
               </li>
               {/* Élément de navigation vers la page 'À propos' */}
               <li>
-                <Link to="/about" onClick={() => handleNavClick('À Propos')}>
+                <Link
+                  to="/about"
+                  className={location.pathname === '/about' ? 'active' : ''}
+                >
                   À Propos
                 </Link>
               </li>
