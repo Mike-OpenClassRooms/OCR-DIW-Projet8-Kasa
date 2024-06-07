0a4152afe9716ef6c3eb29bc65526eec9d18feb6
Mike Pixel Dev <86058062+mikepixeldev@users.noreply.github.com>
2024-06-08 01:28:32 +0200

    Ajout du composant Collapse dans AboutPage

---
+++ kasa/src/components/Collapse/Collapse.jsx
@@ -0,0 +1,5 @@
+// Ce composant gère les menus déroulants dans la page À Propos et les pages des logements.
+
+const Collapse = () => {};
+
+export default Collapse;
--- kasa/src/pages/AboutPage.jsx
+++ kasa/src/pages/AboutPage.jsx
@@ -1,6 +1,7 @@
 // src/pages/AboutPage.jsx
 
 import Banner from '../components/Banner/Banner';
+import Collapse from '../components/Collapse/Collapse';
 
 const aboutBannerImg = './images/banner-kasa-about.webp';
 
@@ -8,6 +9,7 @@ const AboutPage = () => {
   return (
     <>
       <Banner imageUrl={aboutBannerImg} overlayOpacity={0.3} />
+      <Collapse />
     </>
   );
 };
