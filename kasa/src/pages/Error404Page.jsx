fe0fc6c37432ac1384dcf2879fdb08e3f3d796d8
Mike Pixel Dev <86058062+mikepixeldev@users.noreply.github.com>
2024-06-08 14:04:57 +0200

    Ajout de la redirection automatique vers la page d'accueil
    Au bout de 5 secondes, la page d'erreur redirige automatiquement vers la page d'accueil

--- kasa/src/pages/Error404Page.jsx
+++ kasa/src/pages/Error404Page.jsx
@@ -1,8 +1,23 @@
 // src/pages/Error404Page.jsx
 
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 
 function Error404Page() {
+  // Hook de navigation pour rediriger l'utilisateur
+  const navigate = useNavigate();
+
+  // Utiliser useEffect pour configurer la redirection automatique après 5 secondes
+  useEffect(() => {
+    // Définir un timeout pour la redirection
+    const timer = setTimeout(() => {
+      navigate('/');
+    }, 5000);
+
+    // Nettoyer le timeout si le composant est démonté avant la fin du délai
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
   return (
     <div className="error404">
       <h1>404</h1>
