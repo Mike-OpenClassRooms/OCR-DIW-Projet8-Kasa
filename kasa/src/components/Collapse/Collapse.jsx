82149193ecd111f1c060fe3e910606d4a2917d29
Mike Pixel Dev <86058062+mikepixeldev@users.noreply.github.com>
2024-06-08 05:09:37 +0200

    Gestion de l'animation et de l'ouverture des menus déroulants, et mise à jour de la page à propos

--- kasa/src/components/Collapse/Collapse.jsx
+++ kasa/src/components/Collapse/Collapse.jsx
@@ -1,7 +1,31 @@
 // Ce composant gère les menus déroulants dans la page À Propos et les pages des logements.
 
+import React, { useState } from 'react';
 import './Collapse.scss';
 
-const Collapse = () => {};
+const Collapse = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Fonction pour gérer le clic
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={isOpen ? 'full-collapse' : 'closed-collapse'}>
+      <div className="opened-collapse" onClick={handleToggle}>
+        <h2>{title}</h2>
+        <i
+          className={`fa-solid fa-chevron-up fa-2xl ${
+            isOpen ? 'rotate-chevron' : ''
+          }`}
+        ></i>
+      </div>
+      <div className={`collapse-content ${isOpen ? 'show-content' : ''}`}>
+        {children}
+      </div>
+    </div>
+  );
+};
 
 export default Collapse;
--- kasa/src/pages/AboutPage.jsx
+++ kasa/src/pages/AboutPage.jsx
@@ -1,5 +1,6 @@
 // src/pages/AboutPage.jsx
 
+import React from 'react';
 import Banner from '../components/Banner/Banner';
 import Collapse from '../components/Collapse/Collapse';
 
@@ -9,92 +10,41 @@ const AboutPage = () => {
   return (
     <>
       <Banner imageUrl={aboutBannerImg} overlayOpacity={0.3} />
-      <Collapse />
       <div className="about-benefits">
-        {/* Exemple collapse fermé */}
-        <div className="closed-collapse">
-          <h2>Fiabilité</h2>
-          <i class="fa-solid fa-chevron-up fa-2xl"></i>
-        </div>
-        {/* Exemple collapse ouvert */}
-        <div className="full-collapse">
-          <div className="opened-collapse">
-            <h2>Fiabilité</h2>
-            <i class="fa-solid fa-chevron-down fa-2xl"></i>
-          </div>
-          <div className="collapse-content">
-            <p>
-              Les annonces postées sur Kasa garantissent une fiabilité totale.
-              Les photos sont conformes aux logements, et toutes les
-              informations sont régulièrement vérifiées par nos équipes.
-            </p>
-          </div>
-        </div>
+        <Collapse title="Fiabilité">
+          <p>
+            Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+            photos sont conformes aux logements, et toutes les informations sont
+            régulièrement vérifiées par nos équipes.
+          </p>
+        </Collapse>
 
-        {/* Collapses ouverts de la page à propos */}
+        <Collapse title="Respect">
+          <p>
+            La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+            comportement discriminatoire ou de perturbation du voisinage
+            entrainera une exclusion de notre plateforme.
+          </p>
+        </Collapse>
 
-        {/* Collapse Fiabilité */}
-        <div className="full-collapse">
-          <div className="opened-collapse">
-            <h2>Fiabilité</h2>
-            <i class="fa-solid fa-chevron-down fa-2xl"></i>
-          </div>
-          <div className="collapse-content">
-            <p>
-              Les annonces postées sur Kasa garantissent une fiabilité totale.
-              Les photos sont conformes aux logements, et toutes les
-              informations sont régulièrement vérifiées par nos équipes.
-            </p>
-          </div>
-        </div>
+        <Collapse title="Service">
+          <p>
+            Nos équipes se tiennent à votre disposition pour vous fournir une
+            expérience parfaite. N'hésitez pas à nous contacter si vous avez la
+            moindre question.
+          </p>
+        </Collapse>
 
-        {/* Collapse Respect */}
-        <div className="full-collapse">
-          <div className="opened-collapse">
-            <h2>Respect</h2>
-            <i class="fa-solid fa-chevron-down fa-2xl"></i>
-          </div>
-          <div className="collapse-content">
-            <p>
-              La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
-              comportement discriminatoire ou de perturbation du voisinage
-              entrainera une exclusion de notre plateforme.
-            </p>
-          </div>
-        </div>
-
-        {/* Collapse Service */}
-        <div className="full-collapse">
-          <div className="opened-collapse">
-            <h2>Service</h2>
-            <i class="fa-solid fa-chevron-down fa-2xl"></i>
-          </div>
-          <div className="collapse-content">
-            <p>
-              Nos équipes se tiennent à votre disposition pour vous fournir une
-              expérience parfaite. N'hésitez pas à nous contacter si vous avez
-              la moindre question.
-            </p>
-          </div>
-        </div>
-
-        {/* Collapse Sécurité */}
-        <div className="full-collapse">
-          <div className="opened-collapse">
-            <h2>Sécurité</h2>
-            <i class="fa-solid fa-chevron-down fa-2xl"></i>
-          </div>
-          <div className="collapse-content">
-            <p>
-              La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
-              pour les voyageurs, chaque logement correspond aux critères de
-              sécurité établis par nos services. En laissant une note aussi bien
-              à l'hôte qu'au locataire, cela permet à nos équipes de vérifier
-              que les standards sont bien respectés. Nous organisons également
-              des ateliers sur la sécurité domestique pour nos hôtes.
-            </p>
-          </div>
-        </div>
+        <Collapse title="Sécurité">
+          <p>
+            La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+            pour les voyageurs, chaque logement correspond aux critères de
+            sécurité établis par nos services. En laissant une note aussi bien à
+            l'hôte qu'au locataire, cela permet à nos équipes de vérifier que
+            les standards sont bien respectés. Nous organisons également des
+            ateliers sur la sécurité domestique pour nos hôtes.
+          </p>
+        </Collapse>
       </div>
     </>
   );
